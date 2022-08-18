9ab605ab1525bd3d8256a5cf98ed3451d3bb0b28
abduljabbar15 <abduljabber1532002@gmail.com>
2022-08-19 00:24:36 +0600

    create a header file with stack

---
+++ stack/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "myStack.h"
+using namespace std;
+int main()
+{
+    myStack st;
+    st.push(7);
+    st.push(8);
+    st.push(97);
+    cout << st.topValue();
+    // cout << st.count << endl;
+    return 0;
+}
---
+++ stack/myStack.h
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        value = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+class myStack
+{
+public:
+    Node *head = NULL;
+    Node *top = NULL;
+    int count = 0;
+    // push
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head = top = newNode;
+            this->count++;
+            return;
+        }
+        if (head == top)
+        {
+            top->next = newNode;
+            newNode->prev = head;
+            top = newNode;
+            this->count++;
+            return;
+        }
+        else
+        {
+            top->next = newNode;
+            newNode->prev = head;
+            top = newNode;
+            this->count++;
+            return;
+        }
+    }
+    // pop
+    int pop()
+    {
+        int getValue;
+        Node *deletedValue = top;
+        if (head == NULL)
+        {
+            cout << "No element for pop" << endl;
+            return -1;
+        }
+        getValue = deletedValue->value;
+        if (head == top)
+        {
+            top = head = NULL;
+            delete deletedValue;
+            count--;
+            return getValue;
+        }
+
+        Node *temp = head;
+        while (temp->next != deletedValue)
+        {
+            temp = temp->next;
+        }
+        top = temp;
+        temp->next = NULL;
+        delete deletedValue;
+        count--;
+        return getValue;
+    }
+    // top value
+    int topValue()
+    {
+        if (top == NULL)
+        {
+            cout << "No value founded" << endl;
+            return -1;
+        }
+        return top->value;
+    }
+
+    // empty
+    bool isEmpty()
+    {
+        if (count == 0)
+            return false;
+        else
+            return true;
+    }
+};
